09921e807efb41f7f871c3cf0dbd31680859c2b7
Hasan Saju <hasansaju11@gmail.com>
2021-12-14 22:06:19 +0600

    structured and init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import MainComponent from "./component/MainComponent";
+
+function App() {
+  return (
+    <div>
+      <MainComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/MainComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Card";
+
+function MainComponent() {
+  return (
+    <div>
+      <h1>Hello IUT</h1>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default MainComponent;
