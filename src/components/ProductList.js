2d2f236f8b86196a43b44d532a508b379eecc9f0
Hasan Saju <hasansaju11@gmail.com>
2021-12-29 13:19:07 +0600

    Modularizing the code with component tree

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,48 +1,59 @@
-function initVoteCount() {
-  return Math.floor(Math.random() * 50 + 15);
-}
+// function initVoteCount() {
+//   return Math.floor(Math.random() * 50 + 15);
+// }
 const products = [
   {
     id: 1,
-    title: "The Pickwick Papers",
-    description: "By Charles Dickens.",
+    title: "React",
+    author: "By Robin Wieruch",
+    description:
+      "One of the best book to learn and implement React in real life projects.",
     url: "#",
-    votes: initVoteCount(),
+    votes: 21,
+    price: 150,
     submitterAvatarUrl:
       "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2NTQ1NTM2OTM2MDYwMTQ2/charles-dickens.jpg",
     productImageUrl:
-      "https://kbimages1-a.akamaihd.net/43b06cb3-bbe6-4733-8b42-39774911a9e6/353/569/90/False/the-pickwick-papers-123.jpg",
+      "https://m.media-amazon.com/images/I/511-vIg1HaL._AC_UY327_FMwebp_QL65_.jpg",
   },
   {
     id: 2,
-    title: "Oliver Twist",
-    description: " By Charles Dickens.",
+    title: "Computer Vision",
+    author: " By Richard Harley",
+    description:
+      "One of the best selling Computer Vision book. Help to kickstart your career as Computer Vision Scientist.",
     url: "#",
-    votes: initVoteCount(),
+    votes: 18,
+    price: 200,
     submitterAvatarUrl:
       "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2NTQ1NTM2OTM2MDYwMTQ2/charles-dickens.jpg",
-    productImageUrl: "https://m.media-amazon.com/images/I/51SH9vHXgtL.jpg",
+    productImageUrl:
+      "https://m.media-amazon.com/images/I/61TKTkmD4FL._AC_UY327_FMwebp_QL65_.jpg",
   },
   {
     id: 3,
     title: "Hamlet",
-    description: "By William Shakespeare.",
+    author: "By William Shakespeare.",
+    description: "One of the best selling literature book.",
     url: "#",
-    votes: initVoteCount(),
+    votes: 9,
+    price: 250,
     submitterAvatarUrl:
       "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxNzgzMzkwNzMz/william-shakespeare-194895-1-402.jpg",
     productImageUrl: "https://m.media-amazon.com/images/I/41ogY1y1smL.jpg",
   },
   {
     id: 4,
-    title: "Macbeth",
-    description: "By William Shakespeare.",
+    title: "Will",
+    author: "By Reed Peter",
+    description: "One of the best selling Motivational book.",
     url: "#",
-    votes: initVoteCount(),
+    votes: 10,
+    price: 250,
     submitterAvatarUrl:
       "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxNzgzMzkwNzMz/william-shakespeare-194895-1-402.jpg",
     productImageUrl:
-      "https://images-na.ssl-images-amazon.com/images/I/71mwfvxaqsL.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL254_SR254,254_.jpg",
   },
 ];
 
